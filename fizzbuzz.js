3def1192f17c48c54790b21ceefc9e43cb523c5b
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-01 17:28:59 +0100

    Part 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,34 @@
 // This is our main function
 function fizzbuzz() {
     for (let i = 1; i <= 100; i++) {
-        let output = "";
+        let output = [];
+        let savedOutput = "";
         // Multiple of 3
         if (i % 3 == 0) {
-            output = output + "Fizz";
+            output.push("Fizz");
         }
+        if (i % 13 == 0) {
+            savedOutput = "Fezz";
+            output.push(savedOutput);
+        }
+
         // Multiple of 5
         if (i % 5 == 0) {
-            output = output + "Buzz";
+            output.push("Buzz");
         }
-        // Log the number if the output string is empty,
+        if (i % 7 == 0) {
+            output.push("Bang");
+        }
+        if (i % 11 == 0) {
+            output = [savedOutput, "Bong"];
+        }
+        // Reverse the output if necessary
+        if (i % 17 == 0) {
+            output.reverse();
+        }
+        // Log the number if the output array is empty,
         // otherwise log the output
-        console.log(output == "" ? i : output);
+        console.log(output.length == 0 ? i : output.join(''));
     }
 
 }
