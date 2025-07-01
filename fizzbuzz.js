a9b984cd27a59b0e12bddf38804e752884d9cbf8
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-01 16:00:17 +0100

    Part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,24 @@
 function fizzbuzz() {
     console.log("Hello, World!");
 
-    // Put your code here...
+    for (let i = 1; i <= 100; i++) {
+        // Multiple of 3 and 5
+        if (i % 15 == 0) {
+            console.log("FizzBuzz");
+        }
+        // Multiple of 5
+        else if (i % 5 == 0) {
+            console.log("Buzz");
+        }
+        // Multiple of 3
+        else if (i % 3 == 0) {
+            console.log("Fizz");
+        }
+        // Neither a multiple of 3 nor 5
+        else {
+            console.log(i);
+        }
+    }
 
 }
 
