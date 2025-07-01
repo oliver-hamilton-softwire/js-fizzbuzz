73b7f92a7648ca7c4fd2106d540fa0f6c5eaf765
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-01 16:11:29 +0100

    Uses string concatenation

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,18 @@
 // This is our main function
 function fizzbuzz() {
     for (let i = 1; i <= 100; i++) {
-        // Multiple of 3 and 5
-        if (i % 15 == 0) {
-            console.log("FizzBuzz");
+        let output = "";
+        // Multiple of 3
+        if (i % 3 == 0) {
+            output = output + "Fizz";
         }
         // Multiple of 5
-        else if (i % 5 == 0) {
-            console.log("Buzz");
+        if (i % 5 == 0) {
+            output = output + "Buzz";
         }
-        // Multiple of 3
-        else if (i % 3 == 0) {
-            console.log("Fizz");
-        }
-        // Neither a multiple of 3 nor 5
-        else {
-            console.log(i);
-        }
+        // Log the number if the output string is empty,
+        // otherwise log the output
+        console.log(output == "" ? i : output);
     }
 
 }
