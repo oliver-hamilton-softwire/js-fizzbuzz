428fc0cd1542724f598cf6166c64396fdd054b0f
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-02 10:23:58 +0100

    Part 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,31 +1,52 @@
 // This is our main function
 function fizzbuzz() {
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 3*5*17; i++) {
         let output = [];
-        let savedOutput = "";
-        // Multiple of 3
-        if (i % 3 == 0) {
-            output.push("Fizz");
-        }
-        if (i % 13 == 0) {
-            savedOutput = "Fezz";
-            output.push(savedOutput);
-        }
+        output = addTest(i, 
+            output, 
+            (i) => i % 3 == 0 ,
+            (output) => output.concat(["Fizz"])
+        );
 
-        // Multiple of 5
-        if (i % 5 == 0) {
-            output.push("Buzz");
-        }
-        if (i % 7 == 0) {
-            output.push("Bang");
-        }
-        if (i % 11 == 0) {
-            output = [savedOutput, "Bong"];
-        }
-        // Reverse the output if necessary
-        if (i % 17 == 0) {
-            output.reverse();
-        }
+        output = addTest(i, 
+            output, 
+            (i) => i % 5 == 0 ,
+            (output) => output.concat(["Buzz"])
+        );
+
+        output = addTest(i, 
+            output, 
+            (i) => i % 7 == 0 ,
+            (output) => output.concat(["Bang"])
+        );
+
+        // Adds 'Bong', but ignores any output up to this point
+        output = addTest(i, 
+            output, 
+            (i) => i % 11 == 0 ,
+            _ => ["Bong"]
+        );
+
+        // Add 'Fezz' immediately before the first item beginning with B
+        // If there is no such item, just append 'Fezz' to the end
+        output = addTest(i, 
+            output, 
+            (i) => i % 13 == 0,
+            (output) => {
+                const idx = output.findIndex(s => s.length > 0 && s[0] == 'B');
+                if (idx == -1) {
+                    return output.concat(["Fezz"]);
+                }
+                return output.toSpliced(idx, 0, "Fezz");
+            }
+        );
+
+        output = addTest(i, 
+            output, 
+            (i) => i % 17 == 0 ,
+            (output) => output.toReversed()
+        );
+
         // Log the number if the output array is empty,
         // otherwise log the output
         console.log(output.length == 0 ? i : output.join(''));
@@ -33,7 +54,17 @@ function fizzbuzz() {
 
 }
 
-function addTest(i, desiredMultiple, msg, clear = false) {
+function addTest(i, output, testFunc, actionFunc) {
+    /* If testFunc(i) returns true, then output the result
+    of performing actionFunc on output. Otherwise, return
+    the original output
+    */
+    if (testFunc(i)) {
+        return actionFunc(output);
+    }
+    else {
+        return output;
+    }
     
 }
 
